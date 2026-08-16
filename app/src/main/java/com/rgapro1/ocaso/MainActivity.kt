527234996d2c67b84ec9d0930ea4ff094a310e1b
ricardogarciaabadia-boop <ricardogarciaabadia@gmail.com>
2026-08-16 23:05:05 +0200

    Refactor MainActivity to use TextView directly

--- app/src/main/java/com/rgapro1/ocaso/MainActivity.kt
+++ app/src/main/java/com/rgapro1/ocaso/MainActivity.kt
@@ -1,44 +1,26 @@
-```kotlin
 package com.rgapro1.ocaso
 
+import android.app.Activity
 import android.os.Bundle
 import android.graphics.Color
-import android.view.Gravity
-import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+```
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        val layout = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
-            setPadding(32, 32, 32, 32)
-            setBackgroundColor(Color.WHITE)
-        }
+    val texto = TextView(this)
 
-        val titulo = TextView(this).apply {
-            text = "RgaPro"
-            textSize = 32f
-            setTextColor(Color.BLACK)
-            gravity = Gravity.CENTER
-        }
+    texto.text = "RgaPro\n\nMi Cartera Ocaso"
+    texto.textSize = 28f
+    texto.setTextColor(Color.BLACK)
+    texto.setBackgroundColor(Color.WHITE)
+    texto.setPadding(40, 80, 40, 40)
 
-        val subtitulo = TextView(this).apply {
-            text = "Mi Cartera Ocaso"
-            textSize = 20f
-            setTextColor(Color.DKGRAY)
-            gravity = Gravity.CENTER
-            setPadding(0, 20, 0, 0)
-        }
-
-        layout.addView(titulo)
-        layout.addView(subtitulo)
-
-        setContentView(layout)
-    }
+    setContentView(texto)
 }
 ```
+
+}
