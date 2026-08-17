d055d97c33a91810c213f3a153e2eaa10854ef97
ricardogarciaabadia-boop <ricardogarciaabadia@gmail.com>
2026-08-17 21:44:32 +0200

    Fix Room primary key nullability in DocumentEntity

--- app/src/main/java/com/rgapro1/ocaso/data/local/DocumentEntity.java
+++ app/src/main/java/com/rgapro1/ocaso/data/local/DocumentEntity.java
@@ -1,11 +1,12 @@
 package com.rgapro1.ocaso.data.local;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "documents")
 public class DocumentEntity {
-    @PrimaryKey public String documentId;
+    @NonNull @PrimaryKey public String documentId;
     public String clientId;
     public String productId;
     public String documentType;
